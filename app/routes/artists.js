64103b98a82a64690d8cab0a89e4185a122eb487
Steve McDonald <steve@eastnorthwest.com>
2017-05-25 15:58:01 -0700

    Stage 4 and cleanup ejs

--- app/routes/artists.js
+++ app/routes/artists.js
@@ -3,15 +3,11 @@
 var express = require('express'), router = express.Router();
 
 module.exports = function (artist, album, song) {
-  console.log(artist);
   router.get('/', function(req, res) {
-    console.log(artist);
     var wait = setInterval(function() {
-      console.log(artist);
       if (artist.isLoaded && album.isLoaded && song.isLoaded)
       {
         clearInterval(wait);
-        console.log("artists.js router", artist, album, song);
         var artistWithCount = artist.getArtists().map(function(singleArtist) {
           singleArtist.albumCount = album.getAlbumCount(singleArtist.id);
           return singleArtist;
