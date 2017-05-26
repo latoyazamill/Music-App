896e73137feac93d9ba0e44ae32bfbb7bd7381a7
Steve McDonald <steve@eastnorthwest.com>
2017-05-26 10:09:08 -0700

    Final revisions

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+// load models and load data
+var Artist = require('./model/artist');
+var artist = new Artist();
+artist.loadAll();
+
+var Album = require('./model/album');
+var album = new Album();
+album.loadAll();
+
+var Song = require('./model/song');
+var song = new Song();
+song.loadAll();
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// define routes
+var about = require('./routes/about');
+app.use('/about', about);
+
+var artists = require('./routes/artists')(artist, album, song);
+app.use('/', artists);
+
+var albums = require('./routes/albums')(artist, album, song);
+app.use('/albums', albums);
+
+var songs = require('./routes/songs')(artist, album, song);
+app.use('/songs', songs);
+
+var info = require('./routes/artist')(artist, album, song);
+app.use('/artists', info);
+
+var albumInfo = require('./routes/album')(artist, album, song);
+app.use('/albums', albumInfo);
+
+// setup static files
+app.use(express.static('public'))
+
+app.listen('8080')
+console.log('8080 is listen port');
